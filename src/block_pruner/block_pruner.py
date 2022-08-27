09828daa1e9ae7b75b07e5b51ad9060b7ed1ac00
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-08-27 16:24:06 +0200

    :construction: Add naive solution

---
+++ src/block_pruner/block_pruner.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+
+class BlockPruner:
+    def __init__(self, start: bytes, end: bytes, needle: bytes) -> None:
+        self.start = start
+        self.end = end
+        self.needle = needle
+
+        self._temp: list[bytes] = []
+        self._save: list[bytes] = []
+
+        self._keep_block = True
+        self._inside_block = False
+
+    def prune_file(self, input_file: Path) -> bytes:
+        with open(input_file, "rb") as input_fp:
+            for line in input_fp:
+                self.feed_line(line=line)
+        return b"".join(self._save)
+
+    def feed_line(self, line: bytes) -> None:
+        if line == self.start:
+            self._inside_block = True
+        if self._inside_block:
+            self._temp.append(line)
+            if line == self.needle:
+                self._keep_block = False
+        else:
+            self._save.append(line)
+
+        if line == self.end:
+            self._keep_or_disregard_block()
+
+    def _keep_or_disregard_block(self) -> None:
+        if self._keep_block:
+            self._save.extend(self._temp)
+        self._temp = []
+        self._keep_block = True
+        self._inside_block = False
